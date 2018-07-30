6b9f7ef51eea096387f70576e31801cb7131831f
dengpeng <dengpeng@fangjinsuo.com>
2018-07-30 11:11:47 +0800

    reset.css

--- tolink-front/src/main.js
+++ tolink-front/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import '../static/css/reset.css' //引入全局的reset样式
 
 Vue.config.productionTip = false
 App.mpType = 'app'
@@ -12,15 +13,13 @@ export default {
   config: {
     // 页面前带有 ^ 符号的，会被编译成首页，其他页面可以选填，我们会自动把 webpack entry 里面的入口页面加进去
     pages: [
-      'pages/logs/main',
-      'pages/index/main',
       '^pages/inCome/main',
       'pages/outPut/main'
     ],
     window: {
       backgroundTextStyle: 'light',
       navigationBarBackgroundColor: '#fff',
-      navigationBarTitleText: 'TEXT',
+      navigationBarTitleText: 'WeChat',
       navigationBarTextStyle: 'black'
     },
     tabBar: {
